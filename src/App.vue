4666351172c6c4c85a4e1174fcf2d60d277b7bda
di <sarkisova.d@megahub.tech>
2024-09-11 14:35:56 +0300

    fix (App.vue): delete spare code

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,4 @@
 
 <script setup>
 import DefaultLayout from "./layouts/default.vue";
-// import { useStore } from "vuex";
-// import { onMounted } from "vue";
-
-// const store = useStore();
-// console.log(store.state);
-
-// onMounted(() => {
-//   store.dispatch("fetchCharacters");
-// });
 </script>
